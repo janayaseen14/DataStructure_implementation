109b0e5783445672ecc9632d50808ea8c99cb93a
Jana Yaseen <janayaseen1442005@gmail.com>
2024-12-29 18:10:42 +0200

    apdate

---
+++ MyQueue.java
@@ -0,0 +1,50 @@
+package javaapplication2;
+import java.util.*;
+public class MyQueue<T>{
+
+    MyLinkedList lst;
+
+    public MyQueue() {
+        lst = new MyLinkedList();
+    }
+
+    //size
+    public int size() {
+        return lst.Size();
+    }
+    
+    //isEmpty
+    public boolean isEmpty() {
+        return lst.isEmpty();
+    }
+    
+    //addLast
+    public void enqueue(T val) {
+        lst.add(val);
+    }
+
+    //removeFirst
+    public T dequeue() {
+        if (lst.Size() == 0) {
+            throw new NoSuchElementException();
+        } else {
+            return (T) lst.removeFirst();
+        }
+    }
+
+    //getFirst(peek)
+    public T getfirst() {
+        if (lst.Size() == 0) {
+            throw new NoSuchElementException();
+        } else {
+            return (T) lst.getFirst();
+        }
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return lst.toString();
+    }
+
+}
