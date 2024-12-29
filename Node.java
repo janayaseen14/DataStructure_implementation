506a33c115a0df569ebab295ebe2d9e89d941e42
Jana Yaseen <janayaseen1442005@gmail.com>
2024-12-29 04:24:35 +0200

    apdate

---
+++ Node.java
@@ -0,0 +1,27 @@
+
+package javaapplication2;
+
+public class Node <T>{
+    T val;
+    Node next;
+    Node(T val){
+        this.val = val;
+    }
+
+    public T getVal() {
+        return val;
+    }
+
+    public void setVal(T val) {
+        this.val = val;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+}
